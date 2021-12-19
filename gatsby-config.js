6f372debe266e515bcde70c1ea8b505b25136a83
Cdec <cdecrement@gmail.com>
2021-12-19 23:21:19 +0900

    [add] Add eslint and prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "homepage",
+    title: "プラネタゲームラボ",
   },
   plugins: [
     {
---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Footer: React.FC = function () {
+  return <footer>© プラネタゲームラボ 2021</footer>;
+};
+export default Footer;
