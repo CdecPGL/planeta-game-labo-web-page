92e399f00a1be876f43e5b55b86498797bfccaa0
Cdec <cdecrement@gmail.com>
2021-12-31 00:29:33 +0900

    [add] サイドバー追加

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import SideBar from './SideBar';
 
-type LayoutProps = {
-  sideBarComponent?: React.ReactNode;
-};
-
-const Layout: React.FC<LayoutProps> = ({ children, sideBarComponent }) => {
+const Layout: React.FC = ({ children }) => {
   return (
     <div className='font-body bg-gradient-to-b from-blue-600 to-sky-200'>
       <Header />
       <div className='flex justify-center'>
         <div className='flex flex-1 flex-wrap max-w-full lg:flex-nowrap lg:max-w-screen-lg'>
           <div className='flex-none w-full lg:flex-auto lg:w-3/4'>{children}</div>
-          <div className='flex-none w-full lg:flex-auto lg:w-1/4'>{sideBarComponent}</div>
+          <div className='flex-none w-full lg:flex-auto lg:w-1/4'>
+            <SideBar />
+          </div>
         </div>
       </div>
       <Footer />
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,19 @@
+import { Helmet } from 'react-helmet';
+
+const SideBar: React.FC = () => {
+  return (
+    <div className='pl-4'>
+      <a
+        className='twitter-timeline'
+        data-height='640'
+        href='https://twitter.com/CdecPGL?ref_src=twsrc%5Etfw'
+      >
+        Tweets by CdecPGL
+      </a>
+      <Helmet>
+        <script async src='https://platform.twitter.com/widgets.js' charSet='utf-8'></script>
+      </Helmet>
+    </div>
+  );
+};
+export default SideBar;
