2baf726b3659e0dd59567e9d25fbb67f51434877
Cdec <cdecrement@gmail.com>
2023-08-15 14:42:24 +0900

    [update] Node v18に更新

--- src/__generated__/gatsby-types.ts
+++ src/__generated__/gatsby-types.ts
@@ -4918,11 +4918,6 @@ type GamesJsonSortInput = {
   readonly order: Maybe<ReadonlyArray<Maybe<SortOrderEnum>>>;
 };
 
-type HeaderQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-type HeaderQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title'>> }> };
-
 type PostsQueryVariables = Exact<{
   sin: Maybe<Scalars['String']>;
 }>;
@@ -4934,36 +4929,10 @@ type PostsQuery = { readonly allFile: { readonly nodes: ReadonlyArray<{ readonly
         & { readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createDate' | 'updateDate'>> }
       )> }> } };
 
-type NotificationAreaQueryVariables = Exact<{ [key: string]: never; }>;
+type HeaderQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-type NotificationAreaQuery = { readonly allFile: { readonly nodes: ReadonlyArray<{ readonly childMarkdownRemark: Maybe<(
-        Pick<MarkdownRemark, 'excerpt'>
-        & { link: MarkdownRemark['gatsbyPath'] }
-        & { readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createDate'>> }
-      )> }> } };
-
-type GamesQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-type GamesQuery = { readonly allGamesJson: { readonly nodes: ReadonlyArray<(
-      Pick<GamesJson, 'title' | 'genres' | 'platforms' | 'officialPageUrl' | 'updateDate' | 'currentVersion' | 'catchPhrase' | 'isDeveloping' | 'releaseSchedule'>
-      & { defaultPageUrl: GamesJson['gatsbyPath'] }
-      & { readonly screenShots: Maybe<ReadonlyArray<Maybe<{ readonly childImageSharp: Maybe<Pick<ImageSharp, 'gatsbyImageData'>> }>>> }
-    )> } };
-
-type GameQueryVariables = Exact<{
-  id: Maybe<Scalars['String']>;
-}>;
-
-
-type GameQuery = { readonly gamesJson: Maybe<(
-    Pick<GamesJson, 'title' | 'summary' | 'genres' | 'platforms' | 'officialPageUrl' | 'releaseDate' | 'updateDate' | 'currentVersion' | 'catchPhrase' | 'isDeveloping' | 'releaseSchedule'>
-    & { readonly screenShots: Maybe<ReadonlyArray<Maybe<{ readonly childImageSharp: Maybe<Pick<ImageSharp, 'gatsbyImageData'>> }>>>, readonly stores: Maybe<ReadonlyArray<Maybe<(
-      Pick<GamesJsonStores, 'platform'>
-      & { readonly sites: Maybe<ReadonlyArray<Maybe<Pick<GamesJsonStoresSites, 'name' | 'url'>>>> }
-    )>>> }
-  )> };
+type HeaderQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title'>> }> };
 
 type PostQueryVariables = Exact<{
   id: Maybe<Scalars['String']>;
@@ -4987,6 +4956,28 @@ type IndexQuery = { readonly recommendedGames: { readonly nodes: ReadonlyArray<(
       & { defaultPageUrl: GamesJson['gatsbyPath'] }
       & { readonly screenShots: Maybe<ReadonlyArray<Maybe<{ readonly childImageSharp: Maybe<Pick<ImageSharp, 'gatsbyImageData'>> }>>> }
     )> } };
+
+type GamesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+type GamesQuery = { readonly allGamesJson: { readonly nodes: ReadonlyArray<(
+      Pick<GamesJson, 'title' | 'genres' | 'platforms' | 'officialPageUrl' | 'updateDate' | 'currentVersion' | 'catchPhrase' | 'isDeveloping' | 'releaseSchedule'>
+      & { defaultPageUrl: GamesJson['gatsbyPath'] }
+      & { readonly screenShots: Maybe<ReadonlyArray<Maybe<{ readonly childImageSharp: Maybe<Pick<ImageSharp, 'gatsbyImageData'>> }>>> }
+    )> } };
+
+type GameQueryVariables = Exact<{
+  id: Maybe<Scalars['String']>;
+}>;
+
+
+type GameQuery = { readonly gamesJson: Maybe<(
+    Pick<GamesJson, 'title' | 'summary' | 'genres' | 'platforms' | 'officialPageUrl' | 'releaseDate' | 'updateDate' | 'currentVersion' | 'catchPhrase' | 'isDeveloping' | 'releaseSchedule'>
+    & { readonly screenShots: Maybe<ReadonlyArray<Maybe<{ readonly childImageSharp: Maybe<Pick<ImageSharp, 'gatsbyImageData'>> }>>>, readonly stores: Maybe<ReadonlyArray<Maybe<(
+      Pick<GamesJsonStores, 'platform'>
+      & { readonly sites: Maybe<ReadonlyArray<Maybe<Pick<GamesJsonStoresSites, 'name' | 'url'>>>> }
+    )>>> }
+  )> };
 
 type GatsbyImageSharpFixedFragment = Pick<ImageSharpFixed, 'base64' | 'width' | 'height' | 'src' | 'srcSet'>;
 
@@ -5013,5 +5004,14 @@ type GatsbyImageSharpFluid_withWebp_tracedSVGFragment = Pick<ImageSharpFluid, 't
 type GatsbyImageSharpFluid_noBase64Fragment = Pick<ImageSharpFluid, 'aspectRatio' | 'src' | 'srcSet' | 'sizes'>;
 
 type GatsbyImageSharpFluid_withWebp_noBase64Fragment = Pick<ImageSharpFluid, 'aspectRatio' | 'src' | 'srcSet' | 'srcWebp' | 'srcSetWebp' | 'sizes'>;
+
+type NotificationAreaQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+type NotificationAreaQuery = { readonly allFile: { readonly nodes: ReadonlyArray<{ readonly childMarkdownRemark: Maybe<(
+        Pick<MarkdownRemark, 'excerpt'>
+        & { link: MarkdownRemark['gatsbyPath'] }
+        & { readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createDate'>> }
+      )> }> } };
 
 }
